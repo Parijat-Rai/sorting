ab00bb3c9f1e9b224ab491b3a9012f555eac5599
Parijat Rai <parijatrai02@gmail.com>
2022-02-08 17:35:38 +0530

    New Sort in New Branch

---
+++ src/sorting/NewSort.java
@@ -0,0 +1,37 @@
+package sorting;
+
+public class NewSort {
+	void sort(int arr[])
+	{
+		int n = arr.length;
+
+		for (int i = 0; i < n-1; i++)
+		{
+			int min_idx = i;
+			for (int j = i+1; j < n; j++)
+				if (arr[j] < arr[min_idx])
+					min_idx = j;
+
+			int temp = arr[min_idx];
+			arr[min_idx] = arr[i];
+			arr[i] = temp;
+		}
+	}
+
+	void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i=0; i<n; ++i)
+			System.out.print(arr[i]+" ");
+		System.out.println();
+	}
+
+	public static void main(String args[])
+	{
+		NewSort ob = new NewSort();
+		int arr[] = {33,21,5,68,73};
+		ob.sort(arr);
+		System.out.println("Sorted array");
+		ob.printArray(arr);
+	}
+}
